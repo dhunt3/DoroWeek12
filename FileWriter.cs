23cbf7e79a28d16e110bb7671d6262f956b647f3
Noel <harrisbnoel@gmail.com>
2015-09-12 13:29:25 -0400

    Updated File Writer code with 2 texts files being created

--- FileWriter.cs
+++ FileWriter.cs
@@ -13,10 +13,19 @@ namespace FileWriterDoro
         {
             StreamWriter writer = new StreamWriter("..\\..\\Answers.txt"); // creates a file called Answers.txt in 2 folders
                                                                            // up from the location (root) of the .exe file
-
             using (writer)
             {
                 writer.WriteLine("Hello World");    // write Hello World in the file
+            }
+
+            StreamWriter writer2 = new StreamWriter("..\\..\\Numbers.txt");
+
+            using (writer2)
+            {
+                for(int i = 1; i <= 20; i++)
+                {
+                    writer2.WriteLine(i);
+                }
             }
             Console.ReadLine();
         }
